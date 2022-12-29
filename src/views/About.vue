ebbac4b6d364bc9ea97b8f6e9ab95b3f0e962d12
Blake Edwards <49415344+blakete@users.noreply.github.com>
2022-12-28 23:02:14 -0500

    added initial structure (#3)
    Co-authored-by: Blake Edwards <blake.t.edwards@boeing.com>

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,40 +5,26 @@
             This website contains sets of curated <u>principles</u>, <u>cognitive structures</u>, and <u>mindsets</u>
             that
             resonate with <a href="https://www.linkedin.com/in/blakete/">me</a> and
-            have tremendously benefited my life. I hope you can find benefit from using them too. 😊
+            have tremendously benefited my work and life. I hope you can find benefit from using them too. 😊
         </p>
-
-        <h2>1. Principles</h2>
+        <blockquote class="otro-blockquote">
+            I believe in the discipline of mastering the best of what other people have figured out.
+            <span>Charlie Munger</span>
+        </blockquote>
+        <blockquote class="otro-blockquote">
+            If I have seen further [than others], it is by standing on the shoulders of giants.
+            <span>Isaac Newton</span>
+        </blockquote>
+        <h2>Disclaimer</h2>
         <p>
-            Principles are general rules or guidelines that are meant to be applied in a wide range of
-            situations that are often based on fundamental truths or laws.
+            The content of this web page is curated from various sources and is not original content. All credit goes to
+            the original authors and sources. This page is intended for informational purposes only and is not intended
+            to take credit for the original content.
         </p>
-        <p>
-            They can be used to guide
-            behavior, decision-making, and problem-solving. They are essential for understanding and predicting the
-            behavior of complex systems.
-        </p>
-        <h2>2. Cognitive Structures</h2>
-        <p>Cognitive structures are basic mental patterns used to process information.</p>
-        <h3>Mental Models</h3>
-        <p>
-            Mental models are mental representations of a person's understanding of a particular concept or system. They
-            are simplified versions of reality that allow us to make sense of the world around us and make decisions
-            based on that understanding.
-        </p>
-        <h3>Schemas</h3>
-        <p>
-            Schemas represent our understanding and knowledge of a particular concept or category. They are organized
-            sets of information that help us understand and remember new information by relating it to what we already
-            know. Schemas allow us to interpret and make sense of new experiences by organizing them into familiar
-            patterns and categories.
-        </p>
-        <h2>3. Mindsets</h2>
-        <p>
-            A mindset is a person's attitude or set of beliefs about themselves and the world around them.
-            It can influence how they perceive and respond to new information and experiences, and can shape their
-            behaviors and actions.
-        </p>
+        <h2>References</h2>
+        <ol>
+            <li><a href="https://a.co/d/9CH7PWH">The Great Mental Models Volume 1: General Thinking Concepts</a></li>
+        </ol>
     </div>
 </template>
   
@@ -52,6 +38,48 @@ export default {
   
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+li {
+    font-size: 20px;
+}
 
+
+@import url(https://fonts.googleapis.com/css?family=Open+Sans:400italic);
+
+.otro-blockquote {
+    max-width: 600px;
+    font-size: 1.4em;
+    width: 60%;
+    margin: 40px 0px;
+    font-family: Open Sans;
+    font-style: italic;
+    color: #555555;
+    padding: 1.2em 30px 1.2em 75px;
+    border-left: 8px solid #78C0A8;
+    line-height: 1.6;
+    position: relative;
+    background: #EDEDED;
+}
+
+.otro-blockquote::before {
+    font-family: Arial;
+    content: "\201C";
+    color: #78C0A8;
+    font-size: 4em;
+    position: absolute;
+    left: 10px;
+    top: -10px;
+}
+
+.otro-blockquote::after {
+    content: '';
+}
+
+.otro-blockquote span {
+    display: block;
+    color: #333333;
+    font-style: normal;
+    font-weight: bold;
+    margin-top: 1em;
+}
 </style>
   
---
+++ src/views/MentalModels.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="mental-models">
+        <h1>Mental Models 🤔</h1>
+        <p>
+            Mental models are mental representations of a person's understanding of a particular concept or system. They
+            are simplified versions of reality that allow us to make sense of the world around us and make decisions
+            based on that understanding.
+        </p>
+        <h2>Table of Contents</h2>
+        <ol>
+            <li>The Map is not the Territory</li>
+            <li>Circle of Competence</li>
+            <li>Falsifiability</li>
+            <li>First Principles Thinking</li>
+            <li>Thought Experiment</li>
+            <li>Necessity and Sufficiency</li>
+            <li>Second-Order Thinking</li>
+            <li>Probabilistic Thinking</li>
+            <li>Causation vs. Correlation</li>
+            <li>Inversion</li>
+            <li>Occam's Razor</li>
+            <li>Hanlon's Razor</li>
+        </ol>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MentalModelsPage',
+    props: {}
+}
+</script>
+
+<style scoped>
+ul {
+    /* list-style: none; */
+}
+</style>
