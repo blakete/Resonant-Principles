2ed8c39c8c22a0bb7081fc2296e5869d4e388e17
Blake Edwards <blake.t.edwards@boeing.com>
2022-12-31 12:21:29 -0500

    added TLDR to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,32 @@
   <div>
     <div class="main-page-content">
       <h1>Home ☀️</h1>
-      <p>
-        Welcome to our website, where we strive to provide a valuable resource for those seeking to deepen their
-        understanding of the world around them. Our goal is to help individuals better understand the nature of the
-        universe in order to promote prosperity for all.
-      </p>
-      <p>
-        We offer a clear and intuitive way to access and explore a wide
-        range of information, empowering users to make informed decisions and contribute to the betterment of society.
-        Our website is an ongoing experiment, as we constantly seek to improve and expand upon the knowledge and
-        resources we offer.
-      </p>
+      <section>
+        <h2>TLDR</h2>
+        <p>
+          Our website aims to help people understand the universe and promote prosperity. We offer a range of
+          information in a clear and intuitive way, and constantly seek to improve and expand upon our knowledge and
+          resources.
+        </p>
+      </section>
+
+      <section>
+        <h2>Overview</h2>
+        <p>
+          Welcome, we are striving to provide a valuable resource for those seeking to deepen their
+          understanding of the world around them. Our goal is to help individuals better understand the nature of the
+          universe in order to promote prosperity for all.
+        </p>
+        <p>
+          We offer a clear and intuitive way to access and explore a wide
+          range of information, empowering users to make informed decisions and contribute to the betterment of society.
+        </p>
+        <p>
+          This is an ongoing experiment, as we constantly seek to improve and expand upon the knowledge and
+          resources we offer.
+        </p>
+      </section>
+
     </div>
     <section class='comments' aria-labelledby="comment">
       <h2 id="comment">Comments</h2>
