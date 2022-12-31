915adbe7f21ff5ba93e0ce1e1a1d5bef95a59dbe
Blake Edwards <blake.t.edwards@boeing.com>
2022-12-31 14:47:10 -0500

    working towards adding vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/explore">Explore</router-link> |
-    <router-link to="/blog">Blog</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view style="min-height: 100vh;"/>
+  <v-app>
+    <v-toolbar app>
+      <span class="hidden-sm-and-up">
+        <v-toolbar-side-icon @click="sidebar = !sidebar"> </v-toolbar-side-icon>
+      </span>
+      <v-toolbar-title>
+        <router-link to="/" style="cursor: pointer">
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path">
+          <v-icon left dark>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 
 export default {
   name: 'App',
-  components: {
-  }
+
+  data: () => ({
+    menuItems: [
+      { title: "Home", path: "/", icon: "home" },
+      { title: "Explore", path: "/explore", icon: "face" },
+      { title: "Blog", path: "/blog", icon: "face" },
+      { title: "About", path: "/about", icon: "face" },
+    ],
+  }),
 }
 </script>
-
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 40px;
-} */
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  padding: 20px;
-}
-
-p {
-  font-size: 20px;
-}
-
-h1 {
-  font-size: 32px;
-}
-
-h2 {
-  font-size: 26px;
-}
-
-h3 {
-  font-size: 22px;
-}
-
-.main-page-content {
-  min-height: 100vh;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 import VueDisqus from 'vue-disqus'
 
+loadFonts()
+
 createApp(App)
-    .use(router)
-    .use(VueDisqus, { shortname: 'resonant-principles' })
-    .mount('#app')
+  .use(router)
+  .use(vuetify)
+  .use(VueDisqus, { shortname: 'resonant-principles' })
+  .mount('#app')
