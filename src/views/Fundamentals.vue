3c121b1e7c62b2887e45ecb665e3bc698c16e305
Blake Edwards <49415344+blakete@users.noreply.github.com>
2023-01-03 21:39:20 -0500

    adding container to explore pages (#18)
    Co-authored-by: Blake Edwards <blake.t.edwards@boeing.com>

--- src/views/Fundamentals.vue
+++ src/views/Fundamentals.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="main-page-content">
-        <h1>Fundamentals 📜</h1>
-        <p>
-        </p>
+    <div class="main-page-container">
+        <div class="main-page-content">
+            <h1>Fundamentals 📜</h1>
+            <p>
+            </p>
+        </div>
+        <section class='comments' aria-labelledby="comment">
+            <h2 id="comment">Comments</h2>
+            <Disqus />
+        </section>
     </div>
-    <section class='comments' aria-labelledby="comment">
-      <h2 id="comment">Comments</h2>
-      <Disqus />
-    </section>
+
 </template>
 
 <script>
