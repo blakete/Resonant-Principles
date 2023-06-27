3a91e0a667dd1f9512dba8906b701d5bbc6605f4
Blake Edwards <blake.t.edwards@boeing.com>
2023-06-27 14:39:26 -0400

    new deployment, added explicit region to lightsail push and deploy scripts

--- .aws-lightsail/t_deploy_container_service.sh
+++ .aws-lightsail/t_deploy_container_service.sh
@@ -1,5 +1,5 @@
-aws lightsail create-container-service-deployment \
+aws lightsail create-container-service-deployment --region ca-central-1 \
     --service-name principia-container-service \
     --containers file://./containers.json \
-    --public-endpoint file://./public-endpoint.json
+    --public-endpoint file://./public-endpoint.json 
     
--- .aws-lightsail/t_push_container.sh
+++ .aws-lightsail/t_push_container.sh
@@ -1,4 +1,4 @@
-aws lightsail push-container-image \
+aws lightsail push-container-image --region ca-central-1 \
     --service-name principia-container-service \
     --label web-app-prod-container \
-    --image web-app-prod-container
+    --image web-app-prod-container 
