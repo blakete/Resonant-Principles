e451e575c7b4f3561bf4b162d4cd9da35a23ec13
blakete <blakeedwards823@gmail.com>
2023-06-27 12:00:09 -0400

    added more structure to behavioral patterns

--- src/views/explore/fundamentals/computation/software-engineering/software-design-patterns/BehavioralPatterns.vue
+++ src/views/explore/fundamentals/computation/software-engineering/software-design-patterns/BehavioralPatterns.vue
@@ -5,67 +5,141 @@
             <p>
                 Handle the communication and responsibilities between different objects.
             </p>
+            <div>
+                <h2>Table of Contents</h2>
+                <ol>
+                    <li>
+                        <a href="#object_behavioral">Object Behavioral Patterns</a>
+                        <ol>
+                            <li>
+                                Chain of Responsibility
+                            </li>
+                            <li>
+                                Command
+                            </li>
+                            <li>
+                                Iterator
+                            </li>
+                            <li>
+                                Mediator
+                            </li>
+                            <li>
+                                Memento
+                            </li>
+                            <li>
+                                Observer
+                            </li>
+                            <li>
+                                State
+                            </li>
+                            <li>
+                                Strategy
+                            </li>
+                            <li>
+                                Visitor
+                            </li>
+                        </ol>
+                    </li>
+                    <li>
+                        <a href="#class_behavioral">Class Behavioral</a>
+                        <ol>
+                            <li>
+                                Template Method
+                            </li>
+                            <li>
+                                Interpreter
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </div>
             <br>
-            <ol>
-                <li>
-                    Object Behavioral
-                    <ol>
-                        <li>
-                            Chain of Responsibility
-                        </li>
-                        <li>
-                            Command: using an object to encapsulate all information needed to perform an action or trigger
-                            an event
-                            at a later time.
-                        </li>
-                        <li>
-                            Iterator: a way to access elements of an aggregate object sequentially without exposing the
-                            underlying
+            <div>
+                <div>
+                    <h2><a id="object_behavioral">Object Behavioral Patterns</a></h2>
+                    <div>
+                        <h3>Chain of Responsibility</h3>
+                        <p>
+
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Command</h3>
+                        <p>
+                            Using an object to encapsulate all information needed to perform an action or trigger an event
+                            at a
+                            later time.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Iterator</h3>
+                        <p>
+                            A way to access elements of an aggregate object sequentially without exposing the underlying
                             representation.
-                        </li>
-                        <li>
-                            Mediator: lets you vary object interaction independently by creating an object that encapsulates
-                            how a
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Mediator</h3>
+                        <p>
+                            Lets you vary object interaction independently by creating an object that encapsulates how a
                             set of objects interact.
-                        </li>
-                        <li>
-                            Memento: capture and externalize an object's internal state so that the object can be restored
-                            to this
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Memento</h3>
+                        <p>
+                            Capture and externalize an object's internal state so that the object can be restored to this
                             state later.
-                        </li>
-                        <li>
-                            Observer: define one-to-many dependency between objects so that when one object changes state,
-                            all its
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Observer</h3>
+                        <p>
+                            Define one-to-many dependency between objects so that when one object changes state, all its
                             dependents are notified and updated automatically.
-                        </li>
-                        <li>
-                            State: allows an object to alter its behavior when its internal state changes.
-                        </li>
-                        <li>
-                            Strategy: enables selecting an algorithm at runtime through instructions as to which in a
+                        </p>
+                    </div>
+                    <div>
+                        <h3>State</h3>
+                        <p>
+                            Allows an object to alter its behavior when its internal state changes.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Strategy</h3>
+                        <p>
+                            Enables selecting an algorithm at runtime through instructions as to which in a
                             family of algorithms to use.
-                        </li>
-                        <li>
-                            Visitor
-                        </li>
-                    </ol>
-                </li>
-                <li>
-                    Class Behavioral
-                    <ol>
-                        <li>
-                            Template Method: an algorithm as a skeleton of operations and leave the details to be
-                            implemented by the
-                            child classes. The overall structure and sequence of the algorithm are preserved by the parent
-                            class.
-                        </li>
-                        <li>
-                            Interpreter: specifies how to evaluate sentences in a language.
-                        </li>
-                    </ol>
-                </li>
-            </ol>
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Visitors</h3>
+                        <p>
+
+                        </p>
+                    </div>
+                </div>
+                <br>
+                <div>
+                    <h2><a id="class_behavioral">Class Behavioral</a></h2>
+                    <div>
+                        <h3>Template Method</h3>
+                        <p>
+                            An algorithm as a skeleton of operations and leave the details to be implemented by the child
+                            classes. The overall structure and sequence of the algorithm are preserved by the parent class.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Interpreter</h3>
+                        <p>
+                            Specifies how to evaluate sentences in a language.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
         </div>
+        <br>
         <section class='comments' aria-labelledby="comment">
             <h2 id="comment">Comments</h2>
             <Disqus />
